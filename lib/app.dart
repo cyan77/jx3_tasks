40ef07a298b67639668a753751712ca4ca22693a
cyan77 <614420453@qq.com>
2026-09-12 20:45:05 +0800

    Fix Windows and Android release build

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'data/local_store.dart';
 import 'state/app_state.dart';
